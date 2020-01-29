3d84e8313b33e6745d0c24c5faf945e810fa3e6a
mjahere023@gmail.com <mjahere023@gmail.com>
2020-01-28 19:02:58 -0700

    Created files

---
+++ seed.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employee_tracker_db
+Create DATABASE employee_tracker_db
+
+USE employee_tracker_db
+
+CREATE TABLE department
+    id INT PRIMARY KEY
+    NAME VARCHAR(30)
+    
+
+
+
+
+
+
+
+CREATE TABLE employee (
+    id INTEGER() AUTO_INCREMENT NOT NULL PRIMARY KEY 
+    first_name VARCHAR(30)
+    last_name VARCHAR(30)
+    role_id INTEGER
+    manager_id INTEGER
+
+);
+
